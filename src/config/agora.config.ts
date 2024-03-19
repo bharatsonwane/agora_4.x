2f49b8d9e01d79977b61220984680185faf37177
Bharat Sonwane <bharatrsonwane@gmail.com>
2024-03-19 11:19:14 +0530

    version

--- src/config/agora.config.ts
+++ src/config/agora.config.ts
@@ -8,7 +8,7 @@ try {
 
 const config = {
   // Get your own App ID at https://dashboard.agora.io/
-  appId: localAppId,
+  appId: 'f3b9026ccc2047a4b33fded8beb2845d',
   // Please refer to https://docs.agora.io/en/Agora%20Platform/token
   token: '',
   channelId: 'testdcg',
